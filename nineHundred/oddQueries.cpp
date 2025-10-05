4e098eb8d5f183c6931ca117e72e7cb5d2d05c41
Aditya Prem <aditya.prem2003@gmail.com>
2025-10-06 04:30:56 +0530

    commit

---
+++ nineHundred/oddQueries.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f(i, a, b) for(int i=a;i<b;i++)
+#define all(x) x.begin(),x.end()
+#define vprint(v) for (auto& elem : v) cout << elem << " "; cout << endl;
+#define mprint(m) for (auto it:m) cout<<it.first<<" : "<<it.second<<endl; cout<<endl;
+#define vint vector<int>
+#define vstring vector<string>
+#define vmat vector<vector<int>>
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define mii map<int,int>
+#define mll map<ll,ll>
+
+const int INF = 1e9;
+const ll MOD = 1e9+7;
+
+// the trick herew is to precompute the running sums
+
+int main() {
+    FAST_IO;
+    int t;
+    cin >> t;
+    while(t--){
+        int n,q;
+        cin>>n>>q;
+        vint nums;
+        vint sums;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            sum+=x;
+            nums.push_back(x);
+            sums.push_back(sum);
+        }
+        vprint(sums);
+        for(int i=0;i<q;i++){
+            int l, r, k;
+            cin >> l >> r >> k;
+            int sum1= sums[n-1]- sums[r-1]+sums[l-1];
+            cout<<sum1<<endl;
+        }
+        
+    }
+    return 0;
+}
+//by ad73prem
