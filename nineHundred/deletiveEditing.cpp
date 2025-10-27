01cc38eb347017124e8ced4ecf582269243b27d5
Aditya Prem <aditya.prem2003@gmail.com>
2025-10-27 12:04:19 +0530

    ello

---
+++ nineHundred/deletiveEditing.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f(i, a, b) for(int i=a;i<b;i++)
+#define all(x) x.begin(),x.end()
+#define vprint(v) for (auto& elem : v) cout << elem << " "; cout << endl;
+#define mprint(m) for (auto it:m) cout<<it.first<<" : "<<it.second<<endl; cout<<endl;
+#define vint vector<int>
+#define vstring vector<string>
+#define vmat vector<vector<int>>
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define mii map<int,int>
+#define mll map<ll,ll>
+
+const int INF = 1e9;
+const ll MOD = 1e9+7;
+
+int main() {
+    FAST_IO;
+    int tt;
+    cin >> tt;
+    while(tt--){
+        string s, t;
+        cin>>s>>t;
+        unordered_map<char,int> fs;
+        unordered_map<char,int> ft;
+        for(char ch: s){
+            fs[ch]++;
+        }
+        for(char ch: t){
+            ft[ch]++;
+        }
+
+        int x= s.length();
+        for(int i=x-1;i>=0;i--){
+            char ch= s[i];
+            if(ft[ch]>fs[ch]){
+                cout<<"no"<<endl;
+                break;
+            }
+
+            if(ft[ch]>0){
+                ft[ch]--;
+            }else{
+                s[i]='.';
+            }
+        }
+        // cout<<s<<endl;
+        string ans;
+        for(char ch: s){
+            if(ch!='.'){
+                ans+=ch;
+            }
+        }
+        string res=(ans==t)?"yes":"no";
+        cout<<res<<endl;
+    }
+    return 0;
+}
+//by ad73prem
